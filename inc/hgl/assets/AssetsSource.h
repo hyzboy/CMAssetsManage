ef66ff499eef8dbb43134aba181bb54d6529eced
hyzboy <hyzboy@gmail.com>
2020-01-12 19:33:39 +0800

    add AssetsType/Source files

---
+++ inc/hgl/assets/AssetsSource.h
@@ -0,0 +1,24 @@
+﻿#ifndef HGL_ASSETS_SOURCE_INCLUDE
+#define HGL_ASSETS_SOURCE_INCLUDE
+
+namespace hgl
+{
+    /**
+     * 资产来源
+     */
+    enum class AssetsSource
+    {
+        Memory=0,               ///<内存
+        Filesystem,             ///<文件系统
+        Archive,                ///<打包/压缩档
+        Database,               ///<数据库
+        SaveDevice,             ///<存储设备
+        StreamDevice,           ///<流式设备(如摄像头、麦克风)
+        Network,                ///<网络
+
+        BEGIN_RANGE =Memory,
+        END_RANGE   =Network,
+        RANGE_SIZE  =(END_RANGE-BEGIN_RANGE)+1
+    };//enum class AssetsSource
+}//namespace hgl
+#endif//HGL_ASSETS_SOURCE_INCLUDE
---
+++ inc/hgl/assets/AssetsType.h
@@ -0,0 +1,25 @@
+﻿#ifndef HGL_ASSETS_TYPE_INCLUDE
+#define HGL_ASSETS_TYPE_INCLUDE
+
+namespace hgl
+{
+    /**
+     * 资产类型
+     */
+    enum class AssetsType
+    {
+        Binary=0,               ///<二进制数据
+
+        Text,                   ///<文本
+        Bitmap,                 ///<位图
+        Vector,                 ///<矢量图
+        MIDI,                   ///<MIDI音乐
+        Wave,                   ///<声音数据
+        Video,                  ///<视频数据
+
+        BEGIN_RANGE =Binary,
+        END_RANGE   =Video,
+        RANGE_SIZE  =(END_RANGE-BEGIN_RANGE)+1
+    };//enum class AssetsType
+}//namespace hgl
+#endif//HGL_ASSETS_TYPE_INCLUDE
