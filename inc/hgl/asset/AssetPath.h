0244bff1b60119806f683b54a137cda2cacc17ac
hyzboy <hyzboy@gmail.com>
2025-05-16 01:55:46 +0800

    added AssetPath.h

--- inc/hgl/asset/AssetPath.h
+++ inc/hgl/asset/AssetPath.h
@@ -22,9 +22,44 @@ namespace hgl::asset
      */
     class AssetPath
     {
+        hgl::asset::Location location;///< 资产位置
+        
+        hgl::U8String name;           ///< 资产名称
+        hgl::U8String path;           ///< 资产路径
+
+        hgl::U8String full_path;      ///< 资产完整路径名
 
     public:
 
+        AssetPath()
+        {
+            location=hgl::asset::Location::Asset;
+            name.Clear();
+            path.Clear();
+            full_path.Clear();
+        }
+        AssetPath(const AssetPath &ap)
+        {
+            location=ap.location;
+            name=ap.name;
+            path=ap.path;
+            full_path=ap.full_path;
+        }
+        ~AssetPath()=default;
+
+        void SetLocation(hgl::asset::Location loc) { location=loc; }
+        hgl::asset::Location GetLocation()const { return location; }
+
+        const hgl::U8String &GetName()const { return name; }
+        const hgl::U8String &GetPath()const { return path; }
+        const hgl::U8String &GetFullPath()const { return full_path; }
+
+        const bool FromString(const U8String &)
+        {
+
+        }
+
+    public:
 
     };//class AssetPath
 }//namespace hgl::asset
